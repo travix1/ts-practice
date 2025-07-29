54197e5fea2c9568982123b7033df06d841a5c57
Eric Steadman <es3649@gmail.com>
2025-07-28 21:44:51 -0600

    finished fizzbuzz & eric cooked me

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,36 @@
 // console.log("hello world!")
 
-for (let i = 1; i < 101; i++) {
-  console.log(i)
-  if ( (i % 3) == 0) {console.log("fizz")}
-  if ( (i % 5) == 0) {console.log("buzz")}
+function trav_fizzbuzz() {
+
+    for (let i = 1; i < 101; i++) {
+        if ( ( i % 15) == 0) {
+            console.log("FizzBuzz")
+        }
+        else if ( (i % 5) == 0) {
+            console.log("Buzz")
+        }
+        else if ( (i % 3) == 0) {
+            console.log("Fizz")
+        }
+        else {
+            console.log(i)
+        }
+
+    }
 }
+
+function stringmanip_fizzbuzz() {
+
+    for (let i = 1; i< 101; i++) {
+        let str = ''
+        if (i % 3 == 0) {
+            str += 'fizz'
+        }
+        if (i % 5 == 0) {
+            str += 'buzz'
+        }
+        console.log(str ? str : i)
+    }
+}
+
+stringmanip_fizzbuzz()
